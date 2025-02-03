09f60cf51ab00d278b2a95edb2a05630e061a922
TirthAgro <sam@shaktimanagro.com>
2025-02-03 12:37:33 +0530

    Update WelcomeController.java
    Update by sonu

--- src/main/java/com/springboot/springbootfirstapp/WelcomeController.java
+++ src/main/java/com/springboot/springbootfirstapp/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
     @RequestMapping("/welcome")
     public String Welcome(){
-        return "Hello Nikku";
+        return "Hello TATPL";
 
 
     }
